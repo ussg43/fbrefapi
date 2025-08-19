1e5a73e42ebfc81d50b6f15616147b49dbf7399a
ussg43 <164197303+ussg43@users.noreply.github.com>
2025-08-19 17:36:52 -0400

    revert

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,24 +4,20 @@ import (
 	"fbrefapi/internal/handlers"
 	"fmt"
 	"net/http"
-	"os"
-
+	
 	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-
-
 	log.SetReportCaller(true)
 	r := chi.NewRouter()
 	handlers.Handler(r)
 	
 	fmt.Println("Starting API Service...")
 
-	err2 := http.ListenAndServe(port, r)
-	if err2 != nil {
-		log.Error(err2)
+	err := http.ListenAndServe(":5656", r)
+	if err != nil {
+		log.Error(err)
 	}
 }
